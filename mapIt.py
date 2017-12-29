f55057f1d63fd3f3912f40dd2b6275942e47f14e
James <james.wall@wustl.edu>
2017-12-28 19:20:40 -0500

    added additional functionality to mapit

---
+++ mapIt.py
@@ -0,0 +1,46 @@
+#! c:\Python36-32\python3.exe
+# launches a map in the browser using an address from the command line or clipboard
+# inspired by ATBSWP
+
+import sys
+# print(sys.version) # only use when debugging
+
+import requests, logging, argparse, webbrowser, pyperclip
+
+parser = argparse.ArgumentParser(description='Description of your program')
+parser.add_argument('-l','--locations', help='Use this argument to indicate if you want the location of the input', nargs='+', required=False)
+parser.add_argument('-d','--directions', help='Use this argument to indicate if you want directions to the input locations from your current location', nargs='+', required=False)
+
+args = parser.parse_args()
+
+logging.debug("before if statements")
+
+if args.directions:
+	logging.debug("directions is true")
+	# check number of command line arguments
+	if len(args.directions) > 0:
+		# get address from command line arguments
+		address = ' '.join(args.directions)
+	else:
+		# Get address from clipboard
+		address = pyperclip.paste()
+		print(address)
+
+	# get user's current location.. TODO needs work on accuracy
+	freegeoip = "http://freegeoip.net/json"
+	geo_r = requests.get(freegeoip)
+	geo_json = geo_r.json()
+
+	webbrowser.open('https://www.google.com/maps/dir/' + str(geo_json["latitude"]) + ',' + str(geo_json["longitude"]) + '/' + address)
+elif args.locations:
+	logging.debug("directions is false")
+	# check number of command line arguments
+	if len(args.locations) > 0:
+		# get address from command line arguments
+		address = ' '.join(args.locations[0:])
+	else:
+		# Get address from clipboard
+		address = pyperclip.paste()
+	webbrowser.open('https://www.google.com/maps/place/' + address)
+
+logging.debug("end of program")
