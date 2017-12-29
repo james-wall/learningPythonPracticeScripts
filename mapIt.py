b4cc19f84fe2720e3b166f4dfdfee9d6a3deee7a
James <james.wall@wustl.edu>
2017-12-29 12:18:22 -0500

    modified mapit to include search functionality

--- mapIt.py
+++ mapIt.py
@@ -10,6 +10,7 @@ import requests, logging, argparse, webbrowser, pyperclip
 parser = argparse.ArgumentParser(description='Description of your program')
 parser.add_argument('-l','--locations', help='Use this argument to indicate if you want the location of the input', nargs='+', required=False)
 parser.add_argument('-d','--directions', help='Use this argument to indicate if you want directions to the input locations from your current location', nargs='+', required=False)
+parser.add_argument('-s','--searchArea', help='Use this argument to indicate if you want to search nearby for things', nargs='+', required=False)
 
 args = parser.parse_args()
 
@@ -33,7 +34,6 @@ if args.directions:
 
 	webbrowser.open('https://www.google.com/maps/dir/' + str(geo_json["latitude"]) + ',' + str(geo_json["longitude"]) + '/' + address)
 elif args.locations:
-	logging.debug("directions is false")
 	# check number of command line arguments
 	if len(args.locations) > 0:
 		# get address from command line arguments
@@ -42,5 +42,20 @@ elif args.locations:
 		# Get address from clipboard
 		address = pyperclip.paste()
 	webbrowser.open('https://www.google.com/maps/place/' + address)
+elif args.searchArea:
+	# check number of command line arguments
+	if len(args.searchArea) > 0:
+		# get address from command line arguments
+		address = ' '.join(args.searchArea)
+	else:
+		# Get address from clipboard
+		address = pyperclip.paste()
+		print(address)
+
+	# get user's current location.. TODO needs work on accuracy
+	freegeoip = "http://freegeoip.net/json"
+	geo_r = requests.get(freegeoip)
+	geo_json = geo_r.json()
+	webbrowser.open('https://www.google.com/maps/search/' + address + '/' + str(geo_json["latitude"]) + ',' + str(geo_json["longitude"]) + '/')
 
 logging.debug("end of program")
